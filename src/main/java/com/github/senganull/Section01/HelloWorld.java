a682b7894e7a2ae259515b12626ca4a44a91cbd4
Null <roitogaming@gmail.com>
2025-12-10 23:10:21 +0900

    [add] HelloWorld.java

---
+++ src/main/java/com/github/senganull/Section01/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.github.senganull.Section01;
+
+//HelloWorldというクラス
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World Java!!");
+    }
+}
