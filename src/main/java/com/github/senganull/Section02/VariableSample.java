71c621cdb8a07ad14d4ce05d1a92be0d498f7a94
Null <roitogaming@gmail.com>
2025-12-15 19:30:59 +0900

    [add] 新規クラス実装

--- src/main/java/com/github/senganull/Section02/VariableSample.java
+++ src/main/java/com/github/senganull/Section02/VariableSample.java
@@ -1,4 +1,21 @@
 package com.github.senganull.Section02;
 
 public class VariableSample {
+    public static void main(String[] args) {
+        //変数の宣言と代入
+        int age = 19;
+
+        //String型の箱(文字列)
+        String name = "SengaNull";
+
+        //箱の中身を表示する
+        System.out.println("名前は : " + name);
+        System.out.println("年齢は : " + age);
+
+        //変数の上書き
+        age = 20;
+
+        System.out.println("誕生日が来たので...");
+        System.out.println("年齢が :" + age + "になりました。");
+    }
 }
